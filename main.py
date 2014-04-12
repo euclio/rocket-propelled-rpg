4795eee6ece54160dcc0cc3fa27a92333c357810
Tim Taylor <megamanlegends2@gmail.com>
2014-04-12 07:54:39 -0700

    no, I removed the print statement.

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def main():
 
     while True:
         for event in pygame.event.get():
-            print(event)
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
