e244a730599f7b442dfa77b374a86a15d4bb3c02
Andy Russell <arussell123@gmail.com>
2014-04-11 23:22:53 -0700

    add abstract action class

--- rprpg/battle/action.py
+++ rprpg/battle/action.py
@@ -3,10 +3,26 @@ import abc
 class Action(object):
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, requires_target):
-        self.target = None
-        self.requires_target = requires_target
+    def __init__(self, acts_on, targets):
+        self.acts_on = acts_on
+        self.targets = targets
+
+
+    def execute(self, targets=None):
+        if not targets and self.requires_target:
+            raise ValueError('Attempted to execute an action that requires a '
+                             'target without specifying a target')
+
+        if len(targets) > len(self.targets):
+            raise ValueError('More targets specified than allowed')
+
+        self._apply(targets)
+
+
+    def requires_target(self):
+        return self.targets > 0
+
 
     @abc.abstractmethod
-    def execute(self):
+    def _apply(self, targets):
         pass
--- rprpg/battle/scene.py
+++ rprpg/battle/scene.py
@@ -3,6 +3,7 @@ from .entity import Entity, Player, NPC
 def default_ordering(entity):
     return entity.speed
 
+
 class Scene(object):
     def __init__(self, canvas, entities, order_key=default_ordering):
         if not order_key:
@@ -12,10 +13,12 @@ class Scene(object):
         self.order_key = order_key
         self.canvas = canvas
 
+
     def start(self):
         for entity in sorted(self.entities, key=self.order_key):
             action = entity.next_action()
-            if action.requires_target:
-                target = entity.select_target(self.entities)
-                action.target = target
-            action.execute()
+            if action.requires_target():
+                targets = entity.select_target(self.entities)
+                action.execute(targets)
+            else:
+                action.execute()
