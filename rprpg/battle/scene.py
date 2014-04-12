12196505d34d877cc0c1c8b856539e36d90d4ed6
Andy Russell <arussell123@gmail.com>
2014-04-11 22:42:32 -0700

    add basic battle logic

---
+++ rprpg/battle/scene.py
@@ -0,0 +1,21 @@
+from .entity import Entity, Player, NPC
+
+def default_ordering(entity):
+    return entity.speed
+
+class Scene(object):
+    def __init__(self, canvas, entities, order_key=default_ordering):
+        if not order_key:
+            self.order_key = default_ordering
+
+        self.entities = entities
+        self.order_key = order_key
+        self.canvas = canvas
+
+    def start(self):
+        for entity in sorted(self.entities, key=self.order_key):
+            action = entity.next_action()
+            if action.requires_target:
+                target = entity.select_target(self.entities)
+                action.target = target
+            action.execute()
